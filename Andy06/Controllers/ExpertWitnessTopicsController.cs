830ca9c3eda25d37deddd6b54dd6ed84707d0c31
Stewart Clay <stewartclay166@gmail.com>
2019-05-23 15:55:30 +0200

    changed home

---
+++ Andy06/Controllers/ExpertWitnessTopicsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Andy06.Controllers
+{
+    public class ExpertWitnessTopicsController : Controller
+    {
+        // GET: ExpertWitnessTopics
+        public ActionResult Index()
+        {
+            return View("ExpertWitnessTopicsView");
+        }
+    }
+}
