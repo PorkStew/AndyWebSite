a02a7eb89a3d13ba12421af0e7b4f2676e6419b0
Stewart Clay <stewartclay166@gmail.com>
2019-06-10 14:01:29 +0200

    add footer and more

--- Andy06/Style_Sheet/MenuBar.js
+++ Andy06/Style_Sheet/MenuBar.js
@@ -2,3 +2,10 @@
   const nav = document.querySelector('#navbar');
   if(this.scrollY <= 10) nav.className = ''; else nav.className = 'scroll';
 };
+
+$(function () {
+    $('a[href*=#]').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500000, 'linear');
+    });
+});
